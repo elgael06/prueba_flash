34198025ac04bde5e76ae88f2c8362531dd3f3f1
Cristian Gael Valenzuela Castro <cgvalenzuelac@concredito.com.mx>
2020-10-20 14:08:44 -0600

    build 20-10-20.

---
+++ frontend/build/precache-manifest.752f62ab7c99063f1763cc5d10625c0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9b3069f2e1e3a8dece858d7ab506de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b8c732437cb2de09870",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1b38c181ff8ab82bf9b3",
+    "url": "/static/js/2.6fbf18fa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6fbf18fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8c732437cb2de09870",
+    "url": "/static/js/main.7fb832b3.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
